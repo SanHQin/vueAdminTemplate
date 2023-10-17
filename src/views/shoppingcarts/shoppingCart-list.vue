e17511df26f1fe428cfd70629a6cbd21944024d3
SanHQin <3343521716@qq.com>
2023-10-17 15:26:58 +0800

    first commit

---
+++ src/views/shoppingcarts/shoppingCart-list.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>购物车列表</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShoppingCartList',
+  props: [],
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
